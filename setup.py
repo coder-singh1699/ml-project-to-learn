44c2da235e6397ffc01b5108d804537ee503454b
Saggu, Mankaran - Contractor {} <Mankaran.Saggu.Contractor@pepsico.com>
2026-04-09 12:45:12 +0530

    initail setup of the project

---
+++ setup.py
@@ -0,0 +1,27 @@
+## responsible to create a package for a ML model and upload it to PyPI
+
+from setuptools import setup, find_packages 
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->list[str]:
+    """
+    This function will return the list of requirements mentioned in the requirements.txt file
+    """
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='ml_model_package',
+    version='0.0.1',
+    author='MankaranSingh',
+    author_email='mankaran1699@gmail.com',
+    description='A package for a ML model',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
